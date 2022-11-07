d3fe312935f935d2883f7474fab4799753b61f85
Rashid <rashidyousufzai11@gmail.com>
2022-11-07 10:45:17 -0800

    UPDATED

---
+++ login.php
@@ -0,0 +1,61 @@
+<?php
+
+include 'config.php';
+session_start();
+
+if(isset($_POST['submit'])){
+
+   $email = mysqli_real_escape_string($conn, $_POST['email']);
+   $pass = mysqli_real_escape_string($conn, md5($_POST['password']));
+
+   $select = mysqli_query($conn, "SELECT * FROM `user_form` WHERE email = '$email' AND password = '$pass'") or die('query failed');
+
+   if(mysqli_num_rows($select) > 0){
+      $row = mysqli_fetch_assoc($select);
+      $_SESSION['user_id'] = $row['id'];
+      header('location:home.php');
+   }else{
+      $message[] = 'incorrect email or password!';
+   }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>login</title>
+
+
+</head>
+<body>
+   
+<div class="form-container">
+
+   <form action="" method="post" enctype="multipart/form-data">
+      <div  class="form">
+      <h1>login now</h1>
+      <?php
+      if(isset($message)){
+         foreach($message as $message){
+            echo '<div class="message">'.$message.'</div>';
+         }
+      }
+      ?>
+      <input type="email" name="email" placeholder="enter email" class="box" required>
+      <br>
+      <input type="password" name="password" placeholder="enter password" class="box" required>
+      <br>
+      <input type="submit" name="submit" value="login now" class="btn">
+      <h3>don't have an account? <a href="register.php">regiser now</a></h3>
+      </div>
+   </form>
+
+</div>
+
+</body>
+</html>
