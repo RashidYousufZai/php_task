df6a633e0a118599a5fbbac885c4f8faad0d324f
Rashid <rashidyousufzai11@gmail.com>
2022-11-07 11:19:56 -0800

    updated

--- home.php
+++ home.php
@@ -23,6 +23,7 @@ if(isset($_GET['logout'])){
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>home</title>
+   <link rel="stylesheet" href="css/style.css">
 
 
 </head>
--- update_profile.php
+++ update_profile.php
@@ -55,9 +55,10 @@ if(isset($_POST['update_profile'])){
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>update profile</title>
+   <link rel="stylesheet" href="css/style.css">
 
 </head>
-<body>
+<body class="update_image">
    
 <div class="update-profile">
 
